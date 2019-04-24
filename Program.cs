9d3161cc2f89b7980dcf7aba4b05ab4714c2ebf9
fmachadopiriz <fmachadopiriz@outlook.com>
2019-04-24 17:09:50 -0300

    Actualización de comentarios

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MailSenderApiUCU
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        { 
+            MailSender mailSender = new MailSender("sender@gmail.com", "Nombre");
+            mailSender.GetPassword("Ingrese contraseña: ");
+            bool sent = mailSender.SendMail("receiver@mail.com", "Asunto", "Cuerpo");
+            Console.WriteLine(sent ? "Enviado." : "No enviado.");
+        }
+    }
+}
